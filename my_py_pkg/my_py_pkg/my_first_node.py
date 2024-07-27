75557766c2f6227fc1ba87d5ce5dca51d81b0148
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-07-27 20:46:22 +0700

    add testing callback function

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -4,9 +4,16 @@ from rclpy.node import Node
 class MyNode(Node): 
     def __init__(self): 
         super().__init__("py_test")
+        self.counter = 0
         self.get_logger().info("Hello ROS2!")
+        self.create_timer(0.5, self.timer_callback)
 
 
+    def timer_callback(self):
+        self.counter += 1 
+        self.get_logger().info(f"Testing {self.counter}")
+
+# WILL ALWAYS BE SAME ON ALL PROJECT 
 def main(args=None): 
     rclpy.init(args=args) # start ROS2 communication
     node = MyNode()
