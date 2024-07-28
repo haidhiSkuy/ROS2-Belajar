81315f08468771e9da2848ffae2b9344c1453068
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-07-28 09:37:22 +0700

    C++ Hello World OOP

--- my_cpp_pkg/src/my_first_node.cpp
+++ my_cpp_pkg/src/my_first_node.cpp
@@ -1,11 +1,22 @@
 #include "rclcpp/rclcpp.hpp" 
 
+class MyNode: public rclcpp::Node
+{ 
+    public:
+        MyNode(): Node("cpp_test")
+        {
+            RCLCPP_INFO(this->get_logger(), "Hello ROS2 CPP");  
+        }
+
+    private: 
+
+
+};
+
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-
-    auto node = std::make_shared<rclcpp::Node>("cpp_test"); 
-    RCLCPP_INFO(node->get_logger(), "Hello ROS2 CPP");
+    auto node = std::make_shared<MyNode>(); 
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
