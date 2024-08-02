e8fa149d8717270880b51fc7fa84a6c0fca63948
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-02 13:10:32 +0700

    initial python simple publisher

---
+++ my_py_pkg/my_py_pkg/robot_news_station.py
@@ -0,0 +1,16 @@
+import rclpy # ROS library
+from rclpy.node import Node
+
+class RobotNewsStationNode(Node): 
+    def __init__(self): 
+        super().__init__("robot_news_station")
+
+
+def main(args=None): 
+    rclpy.init(args=args) 
+    node = RobotNewsStationNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__": 
+    main()
