9882a57054118999c9d204e70912516abfbc5467
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-04 08:39:06 +0700

    add create_publisher line

--- my_py_pkg/my_py_pkg/robot_news_station.py
+++ my_py_pkg/my_py_pkg/robot_news_station.py
@@ -5,6 +5,7 @@ class RobotNewsStationNode(Node):
     def __init__(self): 
         super().__init__("robot_news_station")
 
+        self.publisher = self.create_publisher()
 
 def main(args=None): 
     rclpy.init(args=args) 
