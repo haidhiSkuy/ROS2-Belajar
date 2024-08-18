ceecfd07ea802e828205c80b1ced6877ee57f324
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-18 08:55:20 +0700

    add subscriber callback

--- my_py_pkg/my_py_pkg/smartphone.py
+++ my_py_pkg/my_py_pkg/smartphone.py
@@ -1,11 +1,20 @@
 import rclpy 
 from rclpy.node import Node 
+from example_interfaces.msg import String
 
 class Smartphone(Node):
     def __init__(self): 
         super().__init__("smartphone") 
         self.get_logger().info("Smartphone has been started")
+        self.subscriber = self.create_subscription(
+            String, 
+            "robot_news", # publisher name 
+            self.callback_robot_news,
+            10
+        )
 
+    def callback_robot_news(self, msg): 
+        self.get_logger().info(msg.data)
     
 def main(args=None): 
     rclpy.init(args=args)
