6a385f5182491b0330852ae5305e4cbe2bf0ded0
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-20 18:44:08 +0700

    add initial file cpp publisher

---
+++ my_cpp_pkg/src/robot_news_station.cpp
@@ -0,0 +1,23 @@
+#include "rclcpp/rclcpp.hpp" 
+#include <string>
+
+class RobotNewsStationNode: public rclcpp::Node
+{ 
+    public:
+        RobotNewsStationNode(): Node("robot_news_station")
+        {
+            RCLCPP_INFO(this->get_logger(), "Hello ROS2 CPP");  
+
+
+        }
+
+};
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<RobotNewsStationNode>(); 
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
