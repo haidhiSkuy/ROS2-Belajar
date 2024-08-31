a2d1b3ac597042b1e86e08a2ae30dddba64155c3
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-08-31 11:06:14 +0700

    initiate client (no oop)

---
+++ my_py_pkg/my_py_pkg/add_two_ints_client.py
@@ -0,0 +1,17 @@
+import rclpy 
+from rclpy.node import Node 
+from example_interfaces.srv import AddTwoInts 
+
+
+def main(args=None): 
+    rclpy.init(args=args) 
+    node = Node("add_two_ints_client")
+
+    client = node.create_client("add_two_ints")
+    while not client.wait_for_service(1.0): 
+        node.get_logger().warn("Waiting for server")
+
+    rclpy.shutdown()
+
+if __name__ == "__main__": 
+    main()
