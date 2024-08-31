f23a08812a99c815fadddea7ef4e978e3641d5b9
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-08-31 11:13:19 +0700

    fix kurang parameter

--- my_py_pkg/my_py_pkg/add_two_ints_client.py
+++ my_py_pkg/my_py_pkg/add_two_ints_client.py
@@ -7,7 +7,7 @@ def main(args=None):
     rclpy.init(args=args) 
     node = Node("add_two_ints_client")
 
-    client = node.create_client("add_two_ints")
+    client = node.create_client(AddTwoInts, "add_two_ints")
     while not client.wait_for_service(1.0): 
         node.get_logger().warn("Waiting for server")
 
