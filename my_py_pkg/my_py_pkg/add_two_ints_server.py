f355af1f851045230e23dbb5aa0619d225ee0847
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-08-31 11:05:50 +0700

    add callback function

--- my_py_pkg/my_py_pkg/add_two_ints_server.py
+++ my_py_pkg/my_py_pkg/add_two_ints_server.py
@@ -5,8 +5,15 @@ from example_interfaces.srv import AddTwoInts
 class AddTowIntsServerNode(Node): 
     def __init__(self): 
         super().__init__("add_two_ints_server")
-        self.server = self.create_service(AddTwoInts, "add_two_ints")
+        self.server = self.create_service(AddTwoInts, "add_two_ints", self.callback_add_two_ints)
        
+    def callback_add_two_ints(self, request, response): 
+        # see the ros2 interfaces show example_interfaces/srv/AddTwoInts to understand
+        response.sum = request.a + request.b
+
+        response_msg = f"{request.a} + {request.b} = {response.sum}"
+        self.get_logger().info(response_msg)
+        return response
 
 
 def main(args=None): 
