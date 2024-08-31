5573f494d2a7dce41289216f2459d0988a640175
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-08-31 11:24:45 +0700

    request and response

--- my_py_pkg/my_py_pkg/add_two_ints_client.py
+++ my_py_pkg/my_py_pkg/add_two_ints_client.py
@@ -8,8 +8,24 @@ def main(args=None):
     node = Node("add_two_ints_client")
 
     client = node.create_client(AddTwoInts, "add_two_ints")
+    
+    # Wait until service is up
     while not client.wait_for_service(1.0): 
         node.get_logger().warn("Waiting for server")
+
+    # Create request
+    request = AddTwoInts.Request()
+    request.a = 5 
+    request.b = 10 
+
+    future = client.call_async(request) # Send request
+    rclpy.spin_until_future_complete(node, future) # Spin until request is complete
+
+    # Get response
+    try: 
+        response = future.result()
+    except Exception as e: 
+        node.get_logger().error(f"Failed {e}")
 
     rclpy.shutdown()
 
