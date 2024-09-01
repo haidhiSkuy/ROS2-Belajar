003f1b47c852075910c9f643d6be1b06e3ffb9c3
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-09-01 08:14:20 +0700

    oop style

--- my_py_pkg/my_py_pkg/add_two_ints_client.py
+++ my_py_pkg/my_py_pkg/add_two_ints_client.py
@@ -1,33 +1,45 @@
 import rclpy 
+from rclpy import Future
 from rclpy.node import Node 
 from example_interfaces.srv import AddTwoInts 
+from functools import partial
 
+
+class AddTwoIntsClient(Node): 
+    def __init__(self): 
+        super().__init__("add_two_ints_client")
+        self.call_add_two_int_server(3, 7)
+
+
+    def call_add_two_int_server(self, a : int, b : int): 
+        client = self.create_client(AddTwoInts, "add_two_ints")
+        
+        # Wait until service is up
+        while not client.wait_for_service(1.0): 
+            self.get_logger().warn("Waiting for server")
+
+        # Create request
+        request = AddTwoInts.Request()
+        request.a = a
+        request.b = b   
+
+        future = client.call_async(request) 
+        future.add_done_callback(
+            partial(self.callback_add_two_ints, a=a, b=b)
+        )
+
+    def callback_add_two_ints(self, future : Future, a : int, b : int): 
+        # Get response 
+        try: 
+            response = future.result()
+            self.get_logger().info(f"Response : {a} + {b} = {response.sum}")
+        except Exception as e: 
+            self.get_logger().error(f"Failed {e}")
 
 def main(args=None): 
-    rclpy.init(args=args) 
-    node = Node("add_two_ints_client")
-
-    client = node.create_client(AddTwoInts, "add_two_ints")
-    
-    # Wait until service is up
-    while not client.wait_for_service(1.0): 
-        node.get_logger().warn("Waiting for server")
-
-    # Create request
-    request = AddTwoInts.Request()
-    request.a = 5 
-    request.b = 10 
-
-    future = client.call_async(request) # Send request
-    rclpy.spin_until_future_complete(node, future) # Spin until request is complete
-
-    # Get response
-    try: 
-        response = future.result()
-        node.get_logger().info(f"Respose : {response.sum}")
-    except Exception as e: 
-        node.get_logger().error(f"Failed {e}")
-
+    rclpy.init(args=args) # start ROS2 communication
+    node = AddTwoIntsClient()
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == "__main__": 
