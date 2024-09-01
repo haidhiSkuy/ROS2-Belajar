43b95b838f7cacff4e37cd2997489da7247638cc
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-09-01 10:20:30 +0700

    cpp add two ints server

--- my_cpp_pkg/src/add_two_ints_server.cpp
+++ my_cpp_pkg/src/add_two_ints_server.cpp
@@ -7,12 +7,23 @@ class AddTwoIntsServerNode : public rclcpp::Node
     public: 
         AddTwoIntsServerNode() : Node("add_two_ints_server")
         {
+            server = this->create_service<example_interfaces::srv::AddTwoInts>(
+                "add_two_ints", 
+                std::bind(&AddTwoIntsServerNode::CallbackAddTwoInts, this, std::placeholders::_1, std::placeholders::_2)
+            );
+
             RCLCPP_INFO(this->get_logger(), "server start");
-
-
         }
 
     private: 
+        void CallbackAddTwoInts(example_interfaces::srv::AddTwoInts::Request::SharedPtr request, 
+                                example_interfaces::srv::AddTwoInts::Response::SharedPtr response) 
+        { 
+            response->sum = request->a + request->b; 
+            RCLCPP_INFO(this->get_logger(), "%d + %d = %d", request->a, request->b, response->sum);
+        }
+
+        rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr server;
 
 }; 
 
