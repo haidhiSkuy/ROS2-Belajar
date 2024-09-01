bc0507210a2c2c0ec79d5c4aae5beffa1a2b8b1c
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-09-01 15:55:14 +0700

    cpp client oop style

--- my_cpp_pkg/src/add_two_ints_client.cpp
+++ my_cpp_pkg/src/add_two_ints_client.cpp
@@ -2,33 +2,54 @@
 #include <example_interfaces/srv/add_two_ints.hpp> 
 
 
+
+class AddTwoIntsClient : public rclcpp::Node 
+{
+    public: 
+        AddTwoIntsClient() : Node("add_two_ints_client")
+        { 
+            thread_.push_back(std::thread(std::bind(&AddTwoIntsClient::CallAddTwoInts, this, 1, 4)));
+            thread_.push_back(std::thread(std::bind(&AddTwoIntsClient::CallAddTwoInts, this, 2, 5)));
+        };
+
+        void CallAddTwoInts(int a, int b)
+        {
+            auto client = this->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+
+            while (!client->wait_for_service(std::chrono::seconds(1)))
+            {
+                RCLCPP_WARN(this->get_logger(), "server is not started, waiting for server");
+            } 
+
+            auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+            request->a = 1; 
+            request->b = 2;
+
+            auto future = client->async_send_request(request); 
+            try 
+            { 
+                auto response = future.get();
+                RCLCPP_INFO(this->get_logger(), "%d + %d = %ld", a, b, response->sum);
+            }
+            catch (const std::exception &e)
+            {
+                RCLCPP_ERROR(this->get_logger(), "service call failed");
+            }
+
+
+        }
+
+        private: 
+            std::vector<std::thread> thread_;
+};
+
+
 int main(int argc, char** argv) 
 {   
-    rclcpp::init(argc, argv); 
-    auto node = std::make_shared<rclcpp::Node>("add_two_ints_client"); 
-
-    auto client = node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
-
-    while (!client->wait_for_service(std::chrono::seconds(1)))
-    {
-        RCLCPP_WARN(node->get_logger(), "server is not started, waiting for server");
-    } 
-
-    auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-    request->a = 1; 
-    request->b = 2;
-
-    auto future = client->async_send_request(request); 
-    rclcpp::spin_until_future_complete(node, future); 
-
-    if (rclcpp::spin_until_future_complete(node, future) == rclcpp::FutureReturnCode::SUCCESS) {
-        auto response = future.get();
-        RCLCPP_INFO(node->get_logger(), "%ld + %ld = %ld", request->a, request->b, response->sum);
-    } else {
-        RCLCPP_ERROR(node->get_logger(), "Failed to call service add_two_ints");
-    }
-
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<AddTwoIntsClient>(); 
+    rclcpp::spin(node);
     rclcpp::shutdown();
-
     return 0;
+  
 }
