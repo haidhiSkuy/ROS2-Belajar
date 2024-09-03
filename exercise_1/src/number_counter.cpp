5f830320fad0cfbdf62ee6fa541986cd10bb3cfd
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-09-03 19:16:10 +0700

    add reset counter service

--- exercise_1/src/number_counter.cpp
+++ exercise_1/src/number_counter.cpp
@@ -1,6 +1,6 @@
 #include <rclcpp/rclcpp.hpp> 
 #include <example_interfaces/msg/int64.hpp> 
-
+#include <example_interfaces/srv/set_bool.hpp>
 
 
 class NumberCounter : public rclcpp::Node
@@ -9,6 +9,13 @@ class NumberCounter : public rclcpp::Node
         NumberCounter() : Node("number_counter")
         { 
             RCLCPP_INFO(this->get_logger(), "This is number_counter");
+            
+            reset_server = this->create_service<example_interfaces::srv::SetBool>( 
+                "reset_counter", 
+                std::bind(&NumberCounter::ResetCounter, this, std::placeholders::_1, std::placeholders::_2)
+            );
+            
+        
             subscriber = this->create_subscription<example_interfaces::msg::Int64>(
                 "number", 10, std::bind(&NumberCounter::AddNumber, this, std::placeholders::_1)
             ); 
@@ -22,6 +29,19 @@ class NumberCounter : public rclcpp::Node
 
 
     private: 
+        void ResetCounter(example_interfaces::srv::SetBool::Request::SharedPtr request, 
+                          example_interfaces::srv::SetBool::Response::SharedPtr response)
+        {   
+            response->success = request->data;
+            bool is_reset = response->success ? "true" : "false";
+            RCLCPP_INFO(this->get_logger(), "Service is %s", response->success ? "true" : "false");
+
+            if (is_reset == true){
+                counter = 0;
+            } 
+        }
+
+
         void AddNumber(const example_interfaces::msg::Int64::SharedPtr msg)
         { 
             counter += msg->data; 
@@ -40,6 +60,9 @@ class NumberCounter : public rclcpp::Node
         rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscriber; 
         rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher; 
         rclcpp::TimerBase::SharedPtr timer_;
+
+        // reset counter service 
+        rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr reset_server;
 
 };
 
