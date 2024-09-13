d25fbb81a8a94b666fa79b27b3b860e67c50e7ff
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-09-13 19:35:43 +0700

    parameter on cpp

--- exercise_1/src/number_publisher.cpp
+++ exercise_1/src/number_publisher.cpp
@@ -11,15 +11,18 @@ class NumberPublisher: public rclcpp::Node
         { 
             RCLCPP_INFO(this->get_logger(), "This is number_publisher");
 
+            // declare parameter 
+            this->declare_parameter<std::string>("test123", "wkwk");
+            
+            this->declare_parameter<int>("number", 2); 
+            number = this->get_parameter("number").as_int();
+
             publisher = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
 
             timer_ = this->create_wall_timer(
                 std::chrono::seconds(1),
                 std::bind(&NumberPublisher::PublishNumber, this)
             );
-
-            // declare parameter 
-            this->declare_parameter<std::string>("test123", "wkwk");
             
         }
         
@@ -28,9 +31,11 @@ class NumberPublisher: public rclcpp::Node
         void PublishNumber()
         {
             auto msg = example_interfaces::msg::Int64(); 
-            msg.data = 2; 
+            msg.data = number; 
             publisher->publish(msg); 
         }
+
+        int number;
         rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher; 
         rclcpp::TimerBase::SharedPtr timer_;
 }; 
