843a1bd8ba4e6a2b3fdbff817c97c6ef0bcea21c
haidhiSkuy <haidhiangkawijana@gmail.com>
2024-09-14 20:03:04 +0700

    add node in launch file

--- my_robot_bringup/launch/number_app.launch.py
+++ my_robot_bringup/launch/number_app.launch.py
@@ -1,5 +1,19 @@
 from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description(): 
     ld = LaunchDescription()
+
+    number_publisher_node = Node(
+        package="exercise_1", 
+        executable="number_publisher"
+    )
+
+    number_counter_node = Node( 
+        package="exercise_1", 
+        executable="number_counter"
+    )
+
+    ld.add_action(number_publisher_node)
+    ld.add_action(number_counter_node)
     return ld
